944d8e6cf48389851c193ef9000ad5cb8883d6a2
mahmod258 <mahmod.rabee2007@gmail.com>
2022-03-22 21:54:58 +0200

    finish day1

--- src/components/main/country.js
+++ src/components/main/country.js
@@ -1,13 +1,21 @@
 export default function Country({ data }) {
   return (
-    <div className="card cards">
-      <img className="card-img-top" src={data.flags.svg} alt="Card image cap" />
+    <div className=" cards">
+      <img className="card-img-top" src={data.flags.svg} />
       <div className="card-body">
         <a href="#">Go somewhere</a>
         <ul>
-          <li>Population: {data.population}</li>
-          <li>data</li>
-          <li></li>
+          <li>
+            <span>Population: </span> {` `}
+            {data.population}
+          </li>
+          <li>
+            <span>region: </span> {` `} {data.region}
+          </li>
+          <li>
+            <span>Capital: </span> {` `}
+            {data.capital}
+          </li>
         </ul>
       </div>
     </div>

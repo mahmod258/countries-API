0326805c5bd9a0e72069ae842e39a1f4240e8991
mahmod258 <mahmod.rabee2007@gmail.com>
2022-03-22 09:15:47 +0200

    fixing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import Navbar from "./components/navbar/navbar";
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav className="navi">
+      <div className="navbar d-flex align-items-center justify-content-between px-5 ">
+        <p>Where in the world?</p>
+        <button></button>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;

ae0faa0e9a9b6c7afb17c76de4c4dd172ecdaae6
mahmod258 <mahmod.rabee2007@gmail.com>
2022-03-23 22:25:07 +0200

    finally fix the bug

---
+++ src/components/country/country.js
@@ -0,0 +1,2 @@
+function Country() {}
+export default Country;
--- src/components/main/country.js
+++ src/components/main/country.js
@@ -2,10 +2,9 @@ import { Link } from "react-router-dom";
 export default function Country({ data }) {
   return (
     <div className=" cards">
-      <Link href={`/${data.altSpellings[0]}`}>
+      <Link to={`/${data.altSpellings[0]}`}>
         <img className="card-img-top" src={data.flags.svg} />
         <div className="card-body">
-          <p>{data.name.common}</p>
           <ul>
             <li>
               <span>Population: </span> {` `}
